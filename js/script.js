1275fa5aae4dc4d4495b5786d18bc39fb4893897
ashley_h <ashhurd16@gmail.com>
2022-05-22 15:43:24 -0400

    reset JS

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const rate = document.getElementById("numbers");
-
-rate.addEventListener('click', function onClick() {
-    rate.style.backgroundColor = 'hsl(25, 97%, 53%)';
-    rate.style.color = 'white';
-});
-
