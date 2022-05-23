c1cc0824a2b28a78dccc492e77dcdd4c035990b6
ashley_h <ashhurd16@gmail.com>
2022-05-22 21:44:00 -0400

    JS complete

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 const designPage = document.querySelector(".design");
 const thankPage = document.querySelector(".thank-you-state");
 const button = document.querySelector("button");
+const ratingNumbers = document.getElementsByClassName("numbers");
 
-/* button functions */
 
-button.addEventListener("click", () => {
-  if (thankPage.style.display === "none") {
-    thankPage.style.display = "block";
-  } else {
-    thankPage.style.display = "none";
+for (let i = 0; i < ratingNumbers.length; i++) {
+    ratingNumbers[i].addEventListener("click", changeButton);
   }
-  if (designPage.style.display === "block") {
-    designPage.style.display = "none";
-  } else {
-    designPage.style.display = "block";
+  
+  let currentActive = "";
+   
+  function changeButton(e) {
+    let oldActive = document.querySelector(".rating-active");
+    oldActive.classList.remove("rating-active", "rating-selected");
+    e.target.classList.add("rating-active", "rating-selected");
+    currentActive = e.target.innerText;
   }
-});
+  
+
+
+button.addEventListener("click", function () {
+    if (Number(currentActive) !== 0) {
+      document.querySelector(".design").classList.add("hidden");
+      document.querySelector(".thank-you-state").classList.remove("hidden");
+      document.querySelector(
+        ".text-rating"
+      ).innerText = `You selected ${currentActive} out of 5`;
+    }
+  });
